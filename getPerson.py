8e7c9e4d6916052680ad3b282956d765405930f3
Arnaldo Pedroso <arnaldo.pedroso@totvs.com.br>
2019-01-08 23:21:05 -0200

    Added a comment into the code

--- getPerson.py
+++ getPerson.py
@@ -4,5 +4,7 @@ vName = str(input("What's your name? "))
 vAge = int(input('How old are you? ')) 
 
 print('Ok I got it!')
+
+# Masks {} just work in python 3
 print('Hello {} you are {} years old!'.format(vName,vAge))
 print('Nice too meet you.')
