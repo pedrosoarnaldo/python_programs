bf03d0d23d995295c0348583728a7869999d99e7
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2019-01-18 21:47:39 -0200

    Exemplo de iteração

---
+++ iteracao_exemplo.py
@@ -0,0 +1,15 @@
+import time
+
+for i in range(1,11):
+    print('{}'.format(11-i))
+    time.sleep(1)
+
+print('Feliz Ano Novo!!!')
+
+i = 10
+while i >= 1:
+    print('{}'.format(i))
+    time.sleep(1)
+    i = i - 1
+
+print('Feliz Ano Novo!!!')
