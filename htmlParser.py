13c4e8187f9e831a5662e4b22950f7dca5ee29c8
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2019-02-22 22:44:44 -0300

    Alterado o script de parse html para arrumar bem o texto

--- htmlParser.py
+++ htmlParser.py
@@ -1,23 +1,11 @@
-from html.parser import HTMLParser
+
 import urllib.request as urllib2
-# from urllib import urlopen
+import nltk
+from nltk.corpus import stopwords
+import string
 
 import lxml.html as lh
-from lxml.html.clean import clean_html
 from lxml.html.clean import Cleaner as Limpador
-
-
-# https://www.programcreek.com/python/example/63565/lxml.html.clean.Cleaner
-# https://rushter.com/blog/python-fast-html-parser/
-# https://stackoverflow.com/questions/328356/extracting-text-from-html-file-using-python
-
-# funcao que limpa o html, este funcionou bem
-
-def lxml_to_text(html):
-    doc = lh.fromstring(html)
-    doc = clean_html(doc)
-    return doc.text_content()
-
 
 def limpaHTML(html):
     limp = Limpador()
@@ -33,54 +21,35 @@ def limpaHTML(html):
     return doc.text_content()
 
 
-# classe htmlparser, nao funciona muito bem pois deixa os CSS e JavaScript
-class MyHTMLParser(HTMLParser):
-    # Initializing lists
-    lsStartTags = list()
-    lsEndTags = list()
-    lsStartEndTags = list()
-    lsComments = list()
-    lsData = list()
-
-    # HTML Parser Methods
-    def handle_starttag(self, startTag, attrs):
-        self.lsStartTags.append(startTag)
-
-    def handle_endtag(self, endTag):
-        self.lsEndTags.append(endTag)
-
-    def handle_startendtag(self, startendTag, attrs):
-        self.lsStartEndTags.append(startendTag)
-
-    def handle_comment(self, data):
-        self.lsComments.append(data)
-
-    def handle_data(self, data):
-        # print("Encountered some data  :", data)
-        self.lsData.append(data)
-
-
-# cria o parser
-# parser = MyHTMLParser()
-
-u = "https://oglobo.globo.com/brasil/camara-derruba-decreto-sobre-sigilo-de-documentos-impoe-primeira-derrota-ao-governo-23464387"
+u = "https://noticias.uol.com.br/internacional/ultimas-noticias/2019/02/22/venezuela-acao-do-brasil-e-humanitaria-e-nao-visa-politica-diz-porta-voz.htm"
 
 html_page = urllib2.urlopen(u)
-
-# Feeding the content
-# parser.feed(str(html_page.read()))
-
 raw = html_page.read()
-
-# printing the extracted values
-# print("Start tags", parser.lsStartTags)
-# print(“End tags”, parser.lsEndTags)
-# print(“Start End tags”, parser.lsStartEndTags)
-# print(“Comments”, parser.lsComments)
-# print("Data", parser.lsData)
-
-# print(raw)
-# rr = lxml_to_text(raw)
 rr = limpaHTML(raw)
-print(rr)
+
+rr = str(rr).replace('.', '. ')
+rr = str(rr).replace('-', ' ')
+rr = str(rr).replace('/', ' ')
+
+''' precisa excluir 18h55atualizada 119h44:1    '''
+
+''' Tira a pontuação do texto'''
+
+translator = str.maketrans('', '', string.punctuation)
+
+tokens = [str(t).lower().translate(translator) for t in rr.split()]
+clean_tokens = tokens[:]
+
+for token in tokens:
+    if token in stopwords.words('portuguese'):
+        clean_tokens.remove(token)
+    if token.isdigit():
+        clean_tokens.remove(token)
+
+freq = nltk.FreqDist(clean_tokens)
+
+for key, val in freq.items():
+    print(key + ':' + str(val))
+
+freq.plot(10, cumulative=False)
 
