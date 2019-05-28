2c064365ffd0d2bcb3d8916fb4b92e23e1638b33
pedrosoarnaldo <pedroso.arnaldo@gmail.com>
2019-05-28 18:33:51 -0300

    Antes de fazer o parse do html eu verifico se a url já não foi classificada.

--- FakeNews/fake_news.py
+++ FakeNews/fake_news.py
@@ -1,13 +1,32 @@
 import criaDicionario
 import eFake
+import mysql
 
-lurl = ['https://www1.folha.uol.com.br/poder/2019/03/fala-de-bolsonaro-gera-critica-e-mourao-tenta-fazer-de-crise-assunto-interno.shtml']
+mydb = mysql.connector.connect(user='root', password='Zse4nji9123!@#',
+                               host='127.0.0.1',
+                               database='fakenews')
+mycursor = mydb.cursor()
+
+
+lurl = ['https://economia.uol.com.br/noticias/redacao/2019/05/28/renegociacao-dividas-caixa.htm']
+
 
 for url in lurl:
-    print('-'*255)
-    print(f'{url}')
-    print('-' *255)
-    vRetornoDicionario = criaDicionario.criaDicionario(url)
-    vRetornoClassifica = eFake.classificaUrl(url)
+    sql = str(f"SELECT e_fake FROM documentos where url = '{url}'")
+    mycursor.execute(sql)
+    myresult = mycursor.fetchone()
 
-print(f'Cria Dicionario ---> {vRetornoDicionario}. Classifica URL {vRetornoClassifica}.')
+    try:
+        for x in myresult:
+            if x >= 0:
+                if x == 0:
+                    print('Documento é fake!')
+                else:
+                    print('Documento não é fake!')
+    except:
+        print('-'*255)
+        print(f'{url}')
+        print('-' *255)
+        vRetornoDicionario = criaDicionario.criaDicionario(url)
+        vRetornoClassifica = eFake.classificaUrl(url)
+        print(f'Cria Dicionario ---> {vRetornoDicionario}. Classifica URL {vRetornoClassifica}.')
