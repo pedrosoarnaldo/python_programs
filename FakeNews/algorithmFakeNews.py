eda3f6c1a02ff7d76c10e034737af33dfd9d8f6c
pedrosoarnaldo <pedroso.arnaldo@gmail.com>
2019-06-01 14:39:14 -0300

    Tratamento de erro ao abrir url

--- FakeNews/algorithmFakeNews.py
+++ FakeNews/algorithmFakeNews.py
@@ -1,12 +1,17 @@
 from fakenews import fakenews
 from myHmlClass import myHtmlClass
 
-url = "https://educacao.uol.com.br/noticias/2019/06/01/mec-fica-isolado-apos-protestos-dizem-especialistas.htm"
+url = "https://www1.folha.uol.com.br/mercado/2019/06/relator-admite-excluir-estados-e-municipios-de-reforma-da-previdencia.shtml"
 
 t = myHtmlClass()
 text = t.hml_cleaner(url)
 
-f = fakenews()
+try:
+    f = fakenews()
+except:
+    print(f'Error trying to open url: {url}!')
+    exit(0)
+
 dimension_language = f.check_language(url, text)
 dimension_sentiment = f.check_sentiment(url, text)
 dimension_key_words = f.check_key_words(url, text)
