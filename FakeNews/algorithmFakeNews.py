d5100c4bd0b93c45bebac1c1948dd9e7983ac7ad
pedrosoarnaldo <pedroso.arnaldo@gmail.com>
2019-06-04 23:19:29 -0300

    Fake news final project

--- FakeNews/algorithmFakeNews.py
+++ FakeNews/algorithmFakeNews.py
@@ -1,6 +1,8 @@
 from DatabasePool import DatabasePool
 from fakenews import fakenews
 from myHtmlClass import myHtmlClass
+from bson.objectid import ObjectId
+
 
 def get_dimension(paramenter_url):
     try:
@@ -67,49 +69,117 @@ def key_word_score(url_id):
     return len(entityTypeScore), entityTypeScore, len(wikipediaScore), wikipediaScore
 
 
-#List of urls to be analyzed
-l_url = ["https://portal.horanoticia.com/tb12/ob/estimulante2/?src=outbrain&utm_source=outbrain&utm_medium=Casa+Vogue+-+Lazer+e+Cultura&utm_campaign=TBL14-ALL&utm_content=Estimulante+natural+ajuda+milhares+de+pessoas+e+esgota+no+Brasil.+Mas+&utm_term=Casa+Vogue+-+Lazer+e+Cultura&wt_token=afaa265e329f4a278246d3a7065fd76b&wt_campaign=008f53aa547f50767076f975ed421811c8&wt_ad=0002dc946a840e31eb75541d934676a8eb&wt_site=00c78b96442ada3338a091766f2e87bed0&wt_network=NativeAds"]
+def log_classification(p_is_fake):
+    if p_is_fake == 0:
+        print("")
+        print("Document is true")
+        print("")
+    else:
+        print("")
+        print("FAKE NEWS!!!")
+        print("")
 
-for url in l_url:
 
-    f = fakenews()
+# Set fakenews object
+f = fakenews()
 
-    #Reset variables
+# Set database
+d = open_database()
+database = d['fakenews']
+
+#Documents to be analyzed
+documents = [
+    {"url": "https://noticias.uol.com.br/politica/ultimas-noticias/2019/06/04/lula-regime-semiaberto-procuradoria.htm", "is_fake": "0"},
+    {"url": "https://esporte.uol.com.br/futebol/ultimas-noticias/2019/06/04/vice-da-cbf-aposta-que-neymar-pede-dispensa-tem-novo-video-vindo.htm", "is_fake": ""},
+    {"url": "https://carros.uol.com.br/noticias/redacao/2019/06/04/bolsonaro-quer-fim-de-exame-de-drogas-e-multa-menor-por-rodar-sem-capacete.htm", "is_fake": "0"},
+    {"url": "http://guiasaude.me/antiacne-aa9/", "is_fake": ""}
+]
+for doc in documents:
+    url = doc["url"]
+    print("")
+    print(f"Working with url {url}")
+    print("")
+
+    v_is_fake = doc["is_fake"]
+
+    # Define dictionary to be searched
+    dict_url = {"url": url}
+
+    # Set collections
+    url_collection = database['url']
+    data_collection = database['data_to_be_analyzed']
+
+    # Reset variables
     vlanguage_score = 0.0
     vsentiment_score = 0.0
 
-    # Set database
-    d = open_database()
-    database = d['fakenews']
-
-    # Set collection
-    url_collection = database['url']
-
-    #Define dictionary to be searched
-    dict_url = {"url": url}
     d_url = url_collection.find(dict_url)
+    if v_is_fake is not "":
+        if url_collection.find(dict_url).count() > 0:
+            return_is_fake = [x["is_fake"] for x in d_url]
+            log_classification(int(return_is_fake[0]))
+            continue
+        else:
+            # Insert url
+            url_id = url_collection.insert_one(dict_url).inserted_id
 
-    if url_collection.find({"url": url}).count() == 0:
-        url_id = url_collection.insert_one(dict_url).inserted_id
-        print(f"Document url# {url_id} was inserted.")
+            # Get dimensions
+            language, sentiment, key_word = get_dimension(url)
+
+            # Prepare data to be recorded
+            dict_data = {"url_id": url_id, "language": language, "sentiment": sentiment, "key_word": key_word}
+
+            # Inserting data_collection
+            data_to_be_analyzed_id = data_collection.insert_one(dict_data).inserted_id
+
+            # Determine score values
+            vlanguage_score = float(language_score(url_id))
+            vsentiment_score = float(sentiment_score(url_id))
+            total_entityTypeScore, entityTypeScore, total_wikipediaScore, wikipediaScore = key_word_score(url_id)
+
+            sum_entityTypeScore = float(sum(entityTypeScore))
+            avg_entityTypeScore = float(sum_entityTypeScore / total_entityTypeScore)
+
+            sum_wikipediaScore = float(sum(wikipediaScore))
+            avg_wikipediaScore = float(sum_wikipediaScore / total_wikipediaScore)
+
+            url_collection.update_one({"url": url}, {"$set":
+                                                         {"language_score": vlanguage_score,
+                                                          "sentiment_score": vsentiment_score,
+                                                          "total_entityTypeScore": total_entityTypeScore,
+                                                          "total_wikipediaScore": total_wikipediaScore,
+                                                          "avg_entityTypeScore": avg_entityTypeScore,
+                                                          "avg_wikipediaScore": avg_wikipediaScore,
+                                                          "is_fake": v_is_fake
+                                                          }
+                                                     })
+
+            print(f"URL: {url} has been inserted.")
+            print(f"URL id# is {url_id}.")
+            print(f"Data id# is {data_to_be_analyzed_id}.")
+            print("")
+    elif url_collection.find(dict_url).count() > 0:
+            return_is_fake = [x["is_fake"] for x in d_url]
+            log_classification(int(return_is_fake[0]))
+            continue
     else:
-        c = url_collection.find(dict_url)
-        url_id = c[0]["_id"]
+        url_swap_collection = database['url_swap']
 
-    #Set collection of data to be analyzed
-    data_collection = database['data_to_be_analyzed']
+        #Insert url_swap
+        url_id = url_swap_collection.insert_one(dict_url).inserted_id
 
-    #Verify if the url was analyzed already
-    if data_collection.find({"url_id": url_id}).count() == 0:
+        #Get dimensions
         language, sentiment, key_word = get_dimension(url)
+
+        #Prepare data to be recorded
         dict_data = {"url_id": url_id, "language": language, "sentiment": sentiment, "key_word": key_word}
-        data_collection.insert_one(dict_data)
-        print(f"Document data_to_be_analyzed# with url# = {url_id} was inserted.")
+
+        #Insert data_collection_swap
+        data_to_be_analyzed_id = data_collection.insert_one(dict_data).inserted_id
 
         #Determine score values
         vlanguage_score = float(language_score(url_id))
         vsentiment_score = float(sentiment_score(url_id))
-        key_word_score(url_id)
         total_entityTypeScore, entityTypeScore, total_wikipediaScore, wikipediaScore = key_word_score(url_id)
 
         sum_entityTypeScore = float(sum(entityTypeScore))
@@ -118,25 +188,29 @@ for url in l_url:
         sum_wikipediaScore = float(sum(wikipediaScore))
         avg_wikipediaScore = float(sum_wikipediaScore / total_wikipediaScore)
 
+        #Verify if the url is fake or not
+        is_fake = f.classify_url([avg_entityTypeScore, avg_wikipediaScore, vlanguage_score, vsentiment_score, sum_entityTypeScore,sum_wikipediaScore])
+
+        #Print classification of the url
+        log_classification(int(is_fake[0]))
+
+        #Remove swap registers
+        url_swap_collection.delete_one({'_id': ObjectId(url_id)})
+        data_collection.delete_one({'_id': ObjectId(data_to_be_analyzed_id)})
+
+        #Inserting correct values
+        url_id = url_collection.insert_one(dict_url).inserted_id
         url_collection.update_one({"url": url}, {"$set":
-            {"language_score": vlanguage_score, "sentiment_score": vsentiment_score,
-                 "total_entityTypeScore": total_entityTypeScore, "total_wikipediaScore": total_wikipediaScore,
-                 "avg_entityTypeScore": avg_entityTypeScore, "avg_wikipediaScore": avg_wikipediaScore,
-                 "is_fake": 1
-            }
-        })
+                                                    {"language_score": vlanguage_score,
+                                                      "sentiment_score": vsentiment_score,
+                                                      "total_entityTypeScore": total_entityTypeScore,
+                                                      "total_wikipediaScore": total_wikipediaScore,
+                                                      "avg_entityTypeScore": avg_entityTypeScore,
+                                                      "avg_wikipediaScore": avg_wikipediaScore,
+                                                      "is_fake": is_fake[0]
+                                                    }
+                                                 })
 
-        is_fake = f.classify_url([avg_entityTypeScore, avg_wikipediaScore, vlanguage_score, vsentiment_score, sum_entityTypeScore, sum_wikipediaScore])
-
-        if is_fake == 0:
-            print("Document is not Fake!")
-        else:
-            print("FAKE NEWS!!!")
-
-    else:
-        dfake = url_collection.find_one({"url": url})
-        if int(dfake["is_fake"]) == 0:
-            print("This document is not fake!")
-        else:
-            print("FAKE NEWS")
-
+        # Inserting data collection
+        dict_data = {"url_id": url_id, "language": language, "sentiment": sentiment, "key_word": key_word}
+        data_to_be_analyzed_id = data_collection.insert_one(dict_data).inserted_id
