52ac43dc24914207230ac5fbce610c92f819c1c8
arnaldopedroso <pedroso.arnaldo@gmail.com>
2020-01-25 12:25:48 -0300

    .

--- neuralNetwork/irisClassifier.py
+++ neuralNetwork/irisClassifier.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 import numpy as np
 import csv
+import neuralNetwork.neuronClassifier as nc
 
 # Parametros das petalas de rosa
 sepala_comprimento = []
@@ -9,7 +10,20 @@ petala_comprimento = []
 petala_largura = []
 classe_da_flor = []
 
-def treeloader(filename):
+# Parametro de resultado da flor
+
+def iris_classifier(iris):
+    if iris == [1, 1]:
+        return "Iris-setosa"
+    elif iris == [-1, 1]:
+        return "Iris-versicolor"
+    elif iris == [-1, -1]:
+        return "Iris-verginica"
+    else:
+        return "unknown"
+
+
+def tree_loader(filename):
     fo = open(filename, 'r')
     lo = csv.reader(fo, delimiter=',')
 
@@ -33,14 +47,7 @@ def treeloader(filename):
 
 dtinicial = datetime.now()
 
-treeloader('iris.data')
-
-
-# Número de épocas
-numEpocas = 1000000
-
-# Número de amostras
-numAmostras = len(classe_da_flor)
+tree_loader('iris.data')
 
 # Atributos
 scomprimento = np.array(sepala_comprimento)
@@ -48,59 +55,19 @@ slargura = np.array(sepala_largura)
 pcomprimento = np.array(petala_comprimento)
 plargura = np.array(petala_largura)
 
-# bias
-bias = 1
-
 # Entrada do Perceptron
 X = np.vstack((scomprimento, slargura, pcomprimento, plargura))
 
-# Classificacao
-Y = np.array(classe_da_flor)
-# Taxa de aprendizado.
-eta = 0.1
-
-# Array para amazernar os erros.
-e = np.ones([numAmostras, 2])
-
-# Define a matriz de pesos.
-W = np.ones([2, 5])         # Quatro entradas e o bias !
-
-
-def funcaoativacao(valor):
-    if valor < 0.0:
-        return -1
-    else:
-        return 1
-
 
 for i in range(2):
-    for j in range(numEpocas):
-        for k in range(numAmostras):
-            # Insere o bias no vetor de entrada.
-            Xb = np.hstack((bias, X[:, k]))
+    result = []
+    for k in classe_da_flor:
+        result.append(k[i])
 
-            # Calcula o vetor campo induzido.
-            V = np.dot(W[i], Xb)
+    [epoch, weight, error] = nc.neuronClassifier.neuron(1, X, result, 0.1, 0.99)
+    print("")
+    print(f"Neuronio {i}")
+    print(f"Epocas: {epoch}")
+    print(f"Peso: {weight}")
+    print(f"Erro: {error}")
 
-            # Degrau bipolar
-            Yr = funcaoativacao(V)
-
-            # Calcula o erro: e = (Y - Yr)
-            e[k][i] = Y[k][i] - Yr
-
-            # Treinando a rede.
-            W[i] = W[i] + eta * e[k][i] * Xb
-
-
-print("Vetor de errors (e) = " + str(e))
-print("Pesos: ", W)
-print("Decorridos: ", datetime.now() - dtinicial)
-
-print("")
-acerto = 0
-for i in range(numAmostras):
-    if e[i][0] == 0 and e[i][0] == e[i][1]:
-        acerto = acerto + 1
-
-tx_acerto = (acerto / numAmostras) * 100
-print ("Taxa de acerto ", tx_acerto)
