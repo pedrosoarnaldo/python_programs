263dccab3e0daa1edc1d97e1dc53d6e851cf6f8e
Arnaldo Pedroso <pedroso.arnaldo@gmail.com>
2020-11-18 21:57:44 -0300

    program made to monitor the amount of sales order

---
+++ PedidosPorMinuto/SalesOrder.py
@@ -0,0 +1,45 @@
+import pyodbc
+import json
+import flask
+
+class SalesOrder:
+    """"Class responsible to get amount of orders from database"""
+
+    def set_api(self):
+        app = flask.Flask(__name__)
+        app.config["DEBUG"] = True
+
+        @app.route('/', methods=['GET'])
+        def home():
+            json_object_return = self.get_order()
+            return json_object_return
+
+        app.run(host = '0.0.0.0')
+
+    def get_order(self):
+
+            amount_orders = {}
+            # Some other example server values are
+            # server = 'localhost\sqlexpress' # for a named instance
+            # server = 'myserver,port' # to specify an alternate port
+
+            server = 'psql01_sec.dc.sbnet\PSQL01_02,1434'
+            database = 'Centauro_Prd'
+            username = 'usr_slack'
+            password = '7eNJWDvGvd2M'
+
+            cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password)
+            cursor = cnxn.cursor()
+            cmd = "SELECT distinct (COUNT(p.Id)/10) AS AVG_10_M,(SELECT distinct COUNT(p.Id) AS AVG_10_M FROM centauro_prd.dbo.Pedidos p (NOLOCK) WHERE p.CriadoEm >= DATEADD(minute,-1,GETDATE()) and p.CriadoEm < GETDATE()) AS last_minute FROM centauro_prd.dbo.Pedidos p (NOLOCK) WHERE p.CriadoEm >= DATEADD(minute,-11,GETDATE()) AND p.CriadoEm < DATEADD(minute,-2,GETDATE())"
+
+            #Sample select query
+            cursor.execute(cmd)
+            row = cursor.fetchone()
+
+            while row:
+                amount_orders['last_10m'] = row[0]
+                amount_orders['last_1m'] = row[1]
+                row = cursor.fetchone()
+
+            json_object = json.dumps(amount_orders, indent=4)
+            return json_object
